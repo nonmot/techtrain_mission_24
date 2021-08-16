d27b8f99cac082bc2930836a6367e2cd2e195a9f
野中元哉 <nonakamotoya@nonakamotoyanoMacBook-Air.local>
2021-08-17 08:41:27 +0900

    detail layout

--- app/javascript/pages/Detail.vue
+++ app/javascript/pages/Detail.vue
@@ -1,8 +1,54 @@
 <template>
     <div class="container px-5 mx-auto">
         <h1 class="text-2xl">架空のアパート</h1>
-        <div class="rounded-lg px-20 h-64 overflow-hidden">
-            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500">
+        <div class="rounded-lg mt-10 px-20 h-auto overflow-hidden">
+            <img alt="content" class="object-cover object-center h-full w-full" src="~images/house.jpg">
+        </div>
+
+        <h1 class="text-2xl mt-20">概要</h1>
+        <div class="flex">
+            <table class="table-fixed flex-grow mt-10">
+                <tbody>
+                    <tr>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                    </tr>
+                    <tr class="bg-gray-100">
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                    </tr>
+                    <tr class="bg-gray-100">
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                        <td class="border px-4 py-2 w-1/4 text-center">間取り</td>
+                        <td class="border py-2 px-4 w-1/4">2LDK</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">間取り</td>
+                        <td class="border py-2 px-4 ">2LDK</td>
+                    </tr>
+                    <tr class="bg-gray-100">
+                        <td class="border px-4 py-2 text-center">間取り</td>
+                        <td class="border py-2 px-4">2LDK</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
